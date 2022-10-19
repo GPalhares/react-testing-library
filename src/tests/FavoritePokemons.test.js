ddb6734b6fb87d57c4dc819ba4d02180bccf2756
Gabriel <devpalhares@gmail.com>
2022-10-19 17:22:16 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('', () => {
+  test('Teste Favoritos', () => {
+    renderWithRouter(<App />);
+
+    const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    userEvent.click(favoritePokemons);
+    const pokeEmpty = screen.getByText('No favorite pokemon found');
+    expect(pokeEmpty).toBeInTheDocument();
+
+    const home = screen.getByRole('link', { name: 'Home' });
+    expect(home).toBeInTheDocument();
+
+    userEvent.click(home);
+    const buttonDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(buttonDetails);
+    const pokemonEscolhido = screen.getByTestId('pokemon-name').textContent;
+    const checkboxFavoritar = screen.getByRole('checkbox');
+    userEvent.click(checkboxFavoritar);
+
+    userEvent.click(favoritePokemons);
+    const nomePokemon = screen.getByText(pokemonEscolhido);
+    expect(nomePokemon).toBeInTheDocument();
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('', () => {
+  test('Teste Componente Pokedex', () => {
+    renderWithRouter(<App />);
+    const encounteredPokemons = screen.getByRole('heading', { name: 'Encountered pokémons' });
+    expect(encounteredPokemons).toBeInTheDocument();
+    const pokemonAtual = screen.getByTestId('pokemon-name').textContent;
+    const buttonNext = screen.getByTestId('next-pokemon');
+    const nomePokemon = screen.getByText(pokemonAtual);
+    userEvent.click(buttonNext);
+
+    expect(nomePokemon).not.toBeInTheDocument();
+  });
+});

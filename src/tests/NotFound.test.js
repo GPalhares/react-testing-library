b0456b7da4e335df4413851a50bafd6278fb1160
Gabriel <devpalhares@gmail.com>
2022-10-19 16:42:57 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('', () => {
+  test('Teste Componente NotFound', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundH2 = screen.getByRole('heading', { name: 'Page requested not found' });
+    expect(notFoundH2).toBeInTheDocument();
+
+    const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
